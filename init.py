fb1722905cf002769c2f2f5543ce1a1a2a1139ae
Patrick Skelley <songkick.skelley@gmail.com>
2021-01-31 11:10:59 -0500

    getting coin history and market chart vs usd

--- init.py
+++ init.py
@@ -1,26 +1,33 @@
 from pycoingecko import CoinGeckoAPI
-import json
-
 
 
 cg = CoinGeckoAPI()
 
-dict = cg.get_search_trending()
+trend = cg.get_search_trending()
 
-for key in dict:
-    print(key, '->', dict[key])
+# coins key is what we need in trend
+for key in trend:
+    print(key, '->', trend[key])
 
-for key in dict:
-    if key == "coins":
-        print("------------------")
-        print("Top 7 searched coins: ")
-        count = 1
-        for dict_2 in dict[key]:
-            print("Info for coin #{}:\n{}".format(count, dict_2))
-            print("")
-            count += 1
+print("------------------")
+print("Top 7 searched coins: ")
+count = 1
+for coin_dict in trend["coins"]:
+    print("Info for coin #{}:\n{}".format(count, coin_dict))
+    print("")
+    count += 1
 
-# j_obj = json.loads(json_data)
-#
-# j_fmt_str = json.dumps(j_obj)
-# print(j_fmt_str)
+print("*************************")
+# not really very useful
+one_inch = cg.get_coin_history_by_id(id='1inch', date='01-12-2020')
+for blah in one_inch:
+    print("{} --> {}".format(blah, one_inch[blah]))
+
+print("*************************")
+# could be useful gives list of arrays for price comparison
+mk_ch = cg.get_coin_market_chart_by_id(id='1inch', vs_currency='usd', days=20)
+for price_pair in mk_ch["prices"]:
+    print(price_pair)
+
+print("*************************")
+
