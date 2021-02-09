23e5ba7c8a07e56cc1c6e6b4be633d9b75f60d1b
Patrick Skelley <songkick.skelley@gmail.com>
2021-02-09 00:54:58 -0500

    turns things into functions. integrates the cryptocompare api for use with social data

--- prototype.py
+++ prototype.py
@@ -11,42 +11,87 @@ import json
 
 
 key_file = 'YTD_API.json'
-api_keys = {}
 with open(key_file, 'r') as f:
-    api_keys = json.loads(f.read())
+    ytd_api_keys = json.loads(f.read())
 
-
+cc_key_file = 'CC_API_KEY_FILE.json'
+with open(cc_key_file, 'r') as f:
+    cc_api_keys = json.loads(f.read())
 
 cg = CoinGeckoAPI()
 
-trend = cg.get_search_trending()
 
-# coins key is what we need in trend
-for key in trend:
-    print(key, '->', trend[key])
+# takes CoinGeckoAPI() obj
+# returns a list of the top 7 searched coins on coingecko
+# returned coins are dicts
+# {
+# 'id': 'string',
+# 'name': 'string',
+# 'symbol': 'string',
+# 'market_cap_rank': int,
+# 'thumb': 'string',
+# 'large': 'string',
+# 'score': int
+# }
 
-print("------------------")
-print("Top 7 searched coins: ")
-count = 1
-for coin_dict in trend["coins"]:
-    print("Info for coin #{}:\n{}".format(count, coin_dict))
-    print("")
-    count += 1
+def get_top_seven_symbol(cgapi):
+    trend = cgapi.get_search_trending()
+    top_seven = []
+    for coin in trend["coins"]:
+        top_seven.append(coin["item"])
+    return top_seven
 
-print("*************************")
-# not really very useful
-one_inch = cg.get_coin_history_by_id(id='1inch', date='01-12-2020')
-for blah in one_inch:
-    print("{} --> {}".format(blah, one_inch[blah]))
+# returns dfi aggregate data
+def get_defi_data(cgapi):
+    return cgapi.get_global_decentralized_finance_defi()
 
-print("*************************")
-# could be useful gives list of arrays for price comparison
-mk_ch = cg.get_coin_market_chart_by_id(id='1inch', vs_currency='usd', days=20)
-for price_pair in mk_ch["prices"]:
-    print(price_pair)
+
+# returns list of dicts
+
+# {
+# 'description': 'string',
+# 'category': 'string',
+# 'created_at': '2021-02-03T17:54:34.274Z',
+# 'user': 'string',
+# 'user_title': 'string',
+# 'pin': boolean,
+# 'project': dict --> {'type': 'string', 'id': 'string', 'name': 'string', 'symbol': 'string', 'image': dict}
+# }
+
+# note category has 'general', 'software release' and 'partnership'
+# we can infer that software release and partnership are good news
+def get_status_updates(cgapi):
+    return cgapi.get_status_updates()["status_updates"]
+
+
+# will return a dictionary where the keys are coin symbols
+# the value is very useful coin information
+# to get coin Id call this function example:
+# inj_id = get_cc_hash()["INJ"]["Id"]
+def get_cc_hash():
+    res = requests.get("https://min-api.cryptocompare.com/data/all/coinlist?api_key={}".format(cc_api_keys['CC_API_KEY']))
+    text_res = res.text
+    j_res = json.loads(text_res)
+    return j_res["Data"]
+
+
+# will get all social data for a coin, must use crypto compare (cc) coin ids
+def get_social(coin_id):
+    res = requests.get("https://min-api.cryptocompare.com/data/social/coin/latest?api_key={}&coinId={}".format(cc_api_keys['CC_API_KEY'], coin_id))
+    text_res = res.text
+    j_res = json.loads(text_res)
+    print(j_res)
+
+
+# returns array of arrays price of asset vs currency
+def get_mk_chart(symbol, vs_currency, num_days, cgapi):
+    mk_chart = cgapi.get_coin_market_chart_by_id(id='1inch', vs_currency='usd', days=20)
+    return mk_chart["prices"]
+
 
 print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-key = api_keys['API_KEY_ID']
+# api key and coin tubers' urls
+key = ytd_api_keys['API_KEY_ID']
 et_URL = "https://www.googleapis.com/youtube/v3/search?" \
          "key={}&" \
          "channelId=UCMtJYS0PrtiUwlk6zjGDEMA&part=snippet,id&order=date&maxResults=20".format(key)
@@ -59,51 +104,61 @@ it_URL = "https://www.googleapis.com/youtube/v3/search?" \
 sm_URL = "https://www.googleapis.com/youtube/v3/search?" \
          "key={}&" \
          "channelId=UCCmJln4C_CszIusbJ_MHmfQ&part=snippet,id&order=date&maxResults=20".format(key)
+
 coin_tubers = {"Elliot": et_URL, "Martini": mg_URL, "Ivan": it_URL, "Suppoman": sm_URL}
-all_vid_info = {}  # coin tuber's last video id dictionary = {name: last_video_id}
-ch_v = json.loads(requests.get(coin_tubers["Elliot"]).text)
-# Look at coin tuber's youtube channel and get the id of their last video
-for tuber in coin_tubers:
-    res = requests.get(coin_tubers[tuber])
-    text_res = res.text
-    channel_vids = json.loads(text_res)
-    latest_vid = channel_vids["items"][1]  # latest video doesn't have subtitles!:/!will have to get 2nd latest([1])...
-    ct_vid_info = {}
-    all_vid_info[tuber] = ct_vid_info
-    ct_vid_info["vid_name"] = latest_vid["snippet"]["title"]
-    ct_vid_info["video_id"] = latest_vid["id"]["videoId"]
+
+# Look at coin tuber's youtube channel and get the id of their *second* to last video
+# get video info - returns dict
+def get_vid_info(ct):
+    all_vid_info = {}
+    for tuber in ct:
+        res = requests.get(ct[tuber])
+        text_res = res.text
+        channel_vids = json.loads(text_res)
+        latest_vid = channel_vids["items"][1]  # latest video doesn't have subtitles!:/!will have to get 2nd latest([1])...
+        ct_vid_info = {}
+        all_vid_info[tuber] = ct_vid_info
+        ct_vid_info["vid_name"] = latest_vid["snippet"]["title"]
+        ct_vid_info["video_id"] = latest_vid["id"]["videoId"]
+    return all_vid_info
+
 
 # Once we have the video's id, we can parse the transcript of the video
-for tuber in all_vid_info:
-    print("\n\n\n")
-    print("Text for {}'s 2nd latest video -------------------> \n title: {}\n".format(tuber, all_vid_info[tuber]["vid_name"]))
-    transcript_obj = YouTubeTranscriptApi.get_transcript(all_vid_info[tuber]["video_id"])
+# takes a tuber's name and a hash of all tubers video info - return value from get_vid_info()
+def create_transcrpt_arry(tuber_name, all_vids):
+    transcript_obj = YouTubeTranscriptApi.get_transcript(all_vids[tuber_name]["video_id"])
     transcript = ""
     for di in transcript_obj:
         transcript += di["text"].lower()
         transcript += " "
-    transcript = transcript.split()
+    return transcript.split()
 
+
+def get_coin_list():
     coin_list = cg.get_coins_list()
     coin_name_list = []
     for coin in coin_list:
+        # coin_name_list.append(coin["symbol"].lower()) # maybe we do not include symbol (ticker) many common words
         coin_name_list.append(coin["name"].lower())
-        # coin_name_list.append(coin["symbol"].lower()) # maybe we do not include symbol (coin ticker) because many common words here i.e. "but"
         coin_name_list.append(coin["id"].lower())
+    return coin_name_list
+
+
+# get the common words (cw) between the two lists from
+# create_transcript_arry() and get_coin_list()
+# generates a list of tupples with 2 values, common word and frequency said word appears in transcript
+def get_cw_trans_coin_lst(transcript, coin_name_list):
     common_words = set(transcript) & set(coin_name_list)
 
-    l_common_words = list(common_words)
-    print("common words are: ")
-    print(l_common_words)
+    cw_list = list(common_words)
 
     word_freq = []
-    for w in transcript:
-        word_freq.append(transcript.count(w))
+    for wd in transcript:
+        word_freq.append(transcript.count(wd))
     words_plus_freq = list(zip(transcript, word_freq))
 
-    print("******************")
-    print("frequency of common word said in video")
-    for common_word in l_common_words:
+    tupple_list_of_freq = []
+    for common_word in cw_list:
         index = transcript.index(common_word)
-        print(words_plus_freq[index])
-    print("------------------------------------------------------------------------------")
+        tupple_list_of_freq.append(words_plus_freq[index])
+    return tupple_list_of_freq
