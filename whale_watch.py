a8ab94dae24942a133945058764889c29d5b6658
Patrick Skelley <songkick.skelley@gmail.com>
2021-03-09 21:39:47 -0500

    adds sms send function and twilio api

--- whale_watch.py
+++ whale_watch.py
@@ -4,19 +4,43 @@ import requests
 from datetime import datetime, timedelta
 from dateutil import tz
 import re
+from twilio.rest import Client
+import os
 
+
+# returns twilio client
+def get_twilio(file):
+    with open(file, 'r') as f:
+        tw_keys = json.loads(f.read())
+    os.environ['TW_SID'] = tw_keys["SID"]
+    os.environ['TW_AUTH_TOKEN'] = tw_keys["AUTH_TOKEN"]
+    tw = Client(tw_keys["SID"], tw_keys["AUTH_TOKEN"])
+    return tw
+
+# bloxy api - ** CAUTION VERY FEW CALLS IN TRIAL (less than 50)**
+def get_bloxy_api(file):
+    with open(file, 'r') as f:
+        bx_key = json.loads(f.read())
+    return bx_key['API_KEY']
 
 # Get API Key for etherscan (needed to get whale's ETH bal)
-with open('ES_API_KEY_FILE.json', 'r') as f:
-    es_key = json.loads(f.read())
-api_key = es_key['ES_API_KEY']
-es = etherscan.Client(
-    api_key=api_key,
-    cache_expire_after=5,
-)
+def get_ether_api(file):
+    with open(file, 'r') as f:
+        es_key = json.loads(f.read())
+    api_key = es_key['ES_API_KEY']
+    es = etherscan.Client(
+        api_key=api_key,
+        cache_expire_after=5,
+    )
+    return es
 
-# get current eth price (not needed at the moment)
-cur_eth_pr = es.get_eth_price()
+def send_sms(tw_client, msg, number):
+    sid = os.environ.get('TW_SID')
+    tw_client.messages.create(
+        messaging_service_sid=sid,
+        body=msg,
+        to=number
+    )
 
 # NFY contract hash for testing
 # contract hashes can be found on the token etherscan pg under contract:
@@ -25,10 +49,6 @@ c_hash = "0x1cbb83ebcd552d5ebf8131ef8c9cd9d9bab342bc"
 # Will use a list of contract hashes, will be extremely small cap tokens
 # list of perhaps 500 (?)
 ctr_hash_list = []
-
-# bloxy api - ** CAUTION VERY FEW CALLS IN TRIAL (less than 50)**
-api = "ACCbkmobSFuMs"
-
 
 # builds a request url to bloxy, returns a string
 # time_interval is a nubmer in minutes
@@ -45,7 +65,6 @@ def build_url(time_interval, ctr_hash, bloxy_api):
            "key={}&format=structure".\
            format(ctr_hash, url_en_xmg_utc_time, bloxy_api)
 
-
 # takes a utc transaction time and converts to nyc time
 def utc_xfr(tx_time):
     from_zone = tz.tzutc()
@@ -56,10 +75,9 @@ def utc_xfr(tx_time):
     utc = utc.replace(tzinfo=from_zone)
     return str(utc.astimezone(to_zone))
 
-
 # takes an individual's eth address and contract decimal precision
 # decimal precision is usually 18
-def get_whale_eth_bal(tx, dec_prec):
+def get_whale_eth_bal(tx, dec_prec, es):
     whale_eth_bal = str(es.get_eth_balance(tx))
     if len(whale_eth_bal) < dec_prec:  # under 0.1 eth
         whale_eth_bal = '0.' + ('0' * (dec_prec - len(whale_eth_bal))) + whale_eth_bal
@@ -84,31 +102,26 @@ def close_conf(file_name, data_update):
     with open(file_name, 'w') as outfile:
         json.dump(data_update, outfile, default=str)
 
-# TODO write function that when called, sends an SMS
-def send_sms_alert():
-    print("THIS IS A TEXT ALERT")
-
 # Takes url and config file
 # returns a processed config file (python dict)
 # return value should be arg to close_conf() to write data
-def process_token_addy(url, conf_file):
+def process_token_addy(url, conf_file, es, tw, numbers):
     r = requests.get(url)
     tx_json_data = json.loads(r.text)
     tx_ct = len(tx_json_data)
     print("\ncount of transcations: {}\n".format(tx_ct))
     tokens = open_conf(conf_file)
+    for token in tokens:
+        tokens[token]["wh_buys_20m"].clear() # always clear out latest tokens list
     if tx_ct == 0:
         print("no whale buys in last 20 minutes")
-        for token in tokens:
-            tokens[token]["wh_buys_20m"].clear() # clearing all from recent buys list
         return tokens
     else:
         for dex_tx in tx_json_data:
             if dex_tx["buySymbol"] == "WETH" and dex_tx["amountBuy"] >= tokens[dex_tx["sellSymbol"]]["eth_whale_thresh"]:
-                # TODO alert! send SMS
 
                 nyc_time = utc_xfr(dex_tx["tx_time"])
-                whale_eth_bal = get_whale_eth_bal(dex_tx["tx_sender"], 18)
+                whale_eth_bal = get_whale_eth_bal(dex_tx["tx_sender"], 18, es)
 
                 tx_dict = {"symbol": dex_tx["sellSymbol"]}
                 keys = {"tx_time" : nyc_time, "amount_buy" : dex_tx["amountBuy"],
@@ -121,11 +134,16 @@ def process_token_addy(url, conf_file):
                 tokens[dex_tx["sellSymbol"]]["all_wh_buys"].append(tx_dict)
 
                 # print info whale to console TODO add logging
-                print("Adding whale buy to conf! \n{} ETH buy \non {} \nfor ticker {}\n"
-                      "whale wallet balance:  {} ETH\nTX HASH: {}\nTX sender: {}".
-                      format(dex_tx["amountBuy"],nyc_time,dex_tx["sellSymbol"],
-                             whale_eth_bal,dex_tx["tx_hash"],dex_tx["tx_sender"]))
-                print("\n************************************************\n")
+                message = "A whale sighting!!! \n{} ETH buy \non {} \nfor ticker {}\nwhale wallet balance:  " \
+                          "{} ETH\nTX HASH: {}\nTX sender: {}\n".format(dex_tx["amountBuy"],nyc_time,dex_tx["sellSymbol"],
+                                                                      whale_eth_bal,dex_tx["tx_hash"],dex_tx["tx_sender"])
+
+                # send sms to Walt and Patrick
+                for num in numbers:
+                    send_sms(tw, message, num)
+
+                # print whale to console
+                print(message + "\n*********************\n")
         return tokens
 
 
--- whale_watch_app.py
+++ whale_watch_app.py
@@ -1,19 +1,4 @@
 import whale_watch
-import json
-import etherscan
-
-
-# Get API Key for etherscan (needed to get whale's ETH bal)
-with open('ES_API_KEY_FILE.json', 'r') as f:
-    es_key = json.loads(f.read())
-api_key = es_key['ES_API_KEY']
-es = etherscan.Client(
-    api_key=api_key,
-    cache_expire_after=5,
-)
-
-# get current eth price (not needed at the moment)
-# cur_eth_pr = es.get_eth_price()
 
 # NFY contract hash for testing
 # contract hashes can be found on the token etherscan pg under contract:
@@ -23,19 +8,14 @@ c_hash = "0x1cbb83ebcd552d5ebf8131ef8c9cd9d9bab342bc"
 # list of perhaps 500 (?)
 ctr_hash_list = []
 
-# bloxy api - ** CAUTION VERY FEW CALLS IN TRIAL (less than 50)**
-api = "ACCbkmobSFuMs"
-
-
-
-
 # note: in the request we will have to figure out the intervals to segment,
 # here I'm segmenting every 60 min in the future we will call build_url
 # in a loop and create a list of low caps to poll
-str_url = whale_watch.build_url(60, c_hash, api)
+ether_api = whale_watch.get_ether_api('ES_API_KEY_FILE.json')
+bx = whale_watch.get_bloxy_api('BLOXY_API.json')
+tw = whale_watch.get_twilio('TWILIO_STUFF.json')
+str_url = whale_watch.build_url(60, c_hash, bx)
 
-
-# TODO:
 # TODO: add text alerting
 # TODO: format buy info for message
 # TODO: add logging
@@ -48,7 +28,7 @@ str_url = whale_watch.build_url(60, c_hash, api)
 # (OPEN CONF FILE ITERATE THROUGH TOKENS AND PROCESS THEIR ADDYS: via building list of ctr hashes
 # then iterate through the list to build each url and have a list of urls, then iterate thr urls and process token adds)
 # uncomment to run
-# new_whale_data = whale_watch.process_token_addy(str_url, 'whale_conf.json')
+new_whale_data = whale_watch.process_token_addy(str_url, 'whale_conf.json', ether_api, tw)
 # print("\n\nnew whale data is")
 # print(new_whale_data)
 # whale_watch.close_conf('whale_conf.json', new_whale_data)
